57f3ec3a9c2881d3845e4dcfe94d1e537380d82a
Blahdidah <80852722+Blahdidah@users.noreply.github.com>
2021-05-17 22:54:29 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static String name = "Melissa";
+
+    static  void sayHello(){
+        System.out.println("Hello, my name is " +name);
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+
+
+    }
+}
